bc80ab2514ef7778bf2ca096c9c7dbd083c87d2d
Martin Wickham <spexguy070@gmail.com>
2017-02-20 20:23:23 -0600

    Add tex loader, callbacks, perf fix

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 
 #include <glm/glm.hpp>
+#include <stb/stb_image.h>
 #include "gl_includes.h"
 #include "Perf.h"
 
@@ -20,6 +21,10 @@ void draw() {
     // draw things
 }
 
+static void glfw_resize_callback(GLFWwindow *window, int width, int height) {
+    printf("resize: %dx%d\n", width, height);
+}
+
 static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (action != GLFW_PRESS) return;
 
@@ -30,6 +35,16 @@ static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int act
         wireframe = !wireframe;
         glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
     }
+}
+
+static void glfw_click_callback(GLFWwindow *window, int button, int action, int mods) {
+    if (action != GLFW_RELEASE) return;
+
+    double x, y;
+    glfwGetCursorPos(window, &x, &y);
+
+    // TODO
+    // mouse clicks
 }
 
 void glfw_error_callback(int error, const char* description) {
@@ -81,6 +96,56 @@ void checkLinkError(GLuint program) {
     delete[] log;
 }
 
+GLuint compileShader(const char *vertSrc, const char *fragSrc) {
+    GLuint vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vertSrc, nullptr);
+    glCompileShader(vertex);
+    checkShaderError(vertex);
+
+    GLuint fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fragSrc, nullptr);
+    glCompileShader(fragment);
+    checkShaderError(fragment);
+
+    GLuint shader = glCreateProgram();
+    glAttachShader(shader, vertex);
+    glAttachShader(shader, fragment);
+    glLinkProgram(shader);
+    checkLinkError(shader);
+
+    return shader;
+}
+
+void loadTexture(GLuint texname, const char *filename) {
+    glBindTexture(GL_TEXTURE_2D, texname);
+
+    int width, height, bpp;
+    unsigned char *pixels = stbi_load(filename, &width, &height, &bpp, STBI_default);
+    if (pixels == nullptr) {
+        cout << "Failed to load image " << filename << " (" << stbi_failure_reason() << ")" << endl;
+        return;
+    }
+    cout << "Loaded " << filename << ", " << height << 'x' << width << ", comp = " << bpp << endl;
+
+    GLenum format;
+    switch(bpp) {
+        case STBI_rgb:
+            format = GL_RGB;
+            break;
+        case STBI_rgb_alpha:
+            format = GL_RGBA;
+            break;
+        default:
+            cout << "Unsupported format: " << bpp << endl;
+            return;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, pixels);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(pixels);
+}
+
 int main() {
     if (!glfwInit()) {
         cout << "Failed to init GLFW" << endl;
@@ -92,12 +157,13 @@ int main() {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Stupid SH Tricks", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "SpexGuy's GLFW Template", NULL, NULL);
     if (!window) {
         cout << "Failed to create window" << endl;
         exit(-1);
     }
     glfwSetKeyCallback(window, glfw_key_callback);
+    glfwSetMouseButtonCallback(window, glfw_click_callback);
 
     glfwMakeContextCurrent(window);
     glewInit();
@@ -110,6 +176,12 @@ int main() {
     glEnable(GL_DEPTH_TEST);
 
     setup();
+    checkError();
+
+    glfwSetFramebufferSizeCallback(window, glfw_resize_callback); // do this after setup
+    int width, height;
+    glfwGetWindowSize(window, &width, &height);
+    glfw_resize_callback(window, width, height); // call resize once with the initial size
 
     // make sure performance data is clean going into main loop
     markPerformanceFrame();
@@ -118,21 +190,22 @@ int main() {
     while (!glfwWindowShouldClose(window)) {
 
         {
+            Perf stat("Poll events");
+            glfwPollEvents();
+            checkError();
+        }
+        {
             Perf stat("Draw");
             draw();
             checkError();
         }
-
         {
             Perf stat("Swap buffers");
             glfwSwapBuffers(window);
             checkError();
         }
-        {
-            Perf stat("Poll events");
-            glfwPollEvents();
-            checkError();
-        }
+
+        markPerformanceFrame();
 
         double now = glfwGetTime();
         if (now - lastPerfPrintTime > 10.0) {
